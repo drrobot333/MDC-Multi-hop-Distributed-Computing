6101325d7e65e42c7a75f6f93e0491c18ba40388
suhwan <drrobot333@naver.com>
2024-11-04 18:02:07 +0900

    fix: Fix zero division error

--- latencymodel/JDPCRA.py
+++ latencymodel/JDPCRA.py
@@ -28,16 +28,25 @@ def _cal_transmission_latency(end_trans_rate, edge_trans_rate, end_data_size, ed
 
 
 def _cal_queueing_latency(arrival_rate, edge_computing, cloud_computing, edge_source, cloud_source):
-    edge_service_rate = edge_source / edge_computing
-    cloud_service_rate = cloud_source / cloud_computing
-    edge_stability = arrival_rate / edge_service_rate
-    cloud_stability = arrival_rate / cloud_service_rate
-    stability_info = (edge_stability, cloud_stability)
-    
-    edge_latency = (arrival_rate / (2*((edge_service_rate)**2)*(1-edge_stability)))
+    edge_latency = 0
     cloud_latency = 0
-    if edge_service_rate > cloud_service_rate:
-        cloud_latency = (arrival_rate / (2*((cloud_service_rate)**2)*(1-cloud_stability)))
     
+    if edge_computing != 0:
+        edge_service_rate = edge_source / edge_computing
+        edge_stability = arrival_rate / edge_service_rate
+        edge_latency = (arrival_rate / (2 * ((edge_service_rate)**2) * (1 - edge_stability)))
+    else:
+        edge_stability = float('inf')
+
+    if cloud_computing != 0:
+        cloud_service_rate = cloud_source / cloud_computing
+        cloud_stability = arrival_rate / cloud_service_rate
+        if edge_computing == 0 or edge_service_rate > cloud_service_rate:
+            cloud_latency = (arrival_rate / (2 * ((cloud_service_rate)**2) * (1 - cloud_stability)))
+    else:
+        cloud_stability = float('inf')
+
+    stability_info = (edge_stability, cloud_stability)
     latency = edge_latency + cloud_latency
+    
     return latency, stability_info
